51451ad253218db720ec7d83fc2002d40cfab6b8
DominoSl <artemenko.mik@gmail.com>
2020-09-15 16:29:36 +0300

    gfth

--- javas/index.js
+++ javas/index.js
@@ -2,16 +2,17 @@ let ks = "https://cors-anywhere.herokuapp.com";
 let rs='https://www.metaweather.com/api/location/924938';
 
 
-// $('#intob').click(()=>{
-//     let rs='https://www.metaweather.com/api/location/search/?query=' + $('#city').val();
-//     fetch(`${ks}/${rs}`)
-//     .then(response => response.json())
-//     .then(json => {
-//          $('#into').html(json.woeid);
-//          rs='https://www.metaweather.com/api/location/search/?query=' + $('#city').val();
-//              })
-//     .catch(err => console.error(err));
-//   })
+ $('#intob').click(()=>{
+    let rs='https://www.metaweather.com/api/location/search/?query=' + $('#city').val();
+    fetch(`${ks}/${rs}`)
+     .then(response => response.json())
+     .then(json => {
+         $('#into').html(json.woeid);
+         $('#ident').val(json.woeid);
+         console.log(json);
+         })
+     .catch(err => console.error(err));
+})
 
 $('#out').click( ()=> {
     fetch(`${ks}/${rs}`)
@@ -22,6 +23,7 @@ $('#out').click( ()=> {
     $('#third').html(Math.round(json.consolidated_weather[2].the_temp) + ' C');
     $('#fourth').html(Math.round(json.consolidated_weather[3].the_temp) + ' C');
     $('#fifth').html(Math.round(json.consolidated_weather[4].the_temp) + ' C');
+    console.log(json);
     })
-    .catch(err => console.error(err));
+    .catch(err => console.error(err));  
   })
