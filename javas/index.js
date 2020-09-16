b35d961972f764f884935e2d12007bf628ff391b
DominoSl <artemenko.mik@gmail.com>
2020-09-16 11:13:11 +0300

    Update index.js

--- javas/index.js
+++ javas/index.js
@@ -5,7 +5,7 @@ $("#intob").click(() => {
   fetch(`${kost}/${real}`)
     .then((response) => response.json())
     .then((json) => {
-      $("#into").html(json[0].woeid);
+      $("#into").html('The id is '+json[0].woeid);
       $("#ident").val(json[0].woeid);
     })
     .catch((err) => console.error(err));
@@ -16,26 +16,16 @@ $("#out").click(() => {
   fetch(`${kost}/${real}`)
     .then((response) => response.json())
     .then((json) => {
-      $("#iconfirst").src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`;  
-      $("#first").html(
-        Math.round(json.consolidated_weather[0].the_temp) + " C"
-      );
-      $("#iconsecond").src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`;  
-      $("#second").html(
-        Math.round(json.consolidated_weather[1].the_temp) + " C"
-      );
-      $("#iconthird").src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`;  
-      $("#third").html(
-        Math.round(json.consolidated_weather[2].the_temp) + " C"
-      );
-      $("#iconfourth").src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`;  
-      $("#fourth").html(
-        Math.round(json.consolidated_weather[3].the_temp) + " C"
-      );
-      $("#iconfifth").src = `https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`;  
-      $("#fifth").html(
-        Math.round(json.consolidated_weather[4].the_temp) + " C"
-      );
+      $("#iconfirst").src(`https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`);  
+      $("#first").html(Math.round(json.consolidated_weather[0].the_temp) + " C");
+      $("#iconsecond").src(`https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`);  
+      $("#second").html(Math.round(json.consolidated_weather[1].the_temp) + " C");
+      $("#iconthird").src(`https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`);  
+      $("#third").html(Math.round(json.consolidated_weather[2].the_temp) + " C");
+      $("#iconfourth").src(`https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`);  
+      $("#fourth").html(Math.round(json.consolidated_weather[3].the_temp) + " C");
+      $("#iconfifth").src(`https://www.metaweather.com/static/img/weather/png/64/${json.consolidated_weather[0].weather_state_abbr}.png`);  
+      $("#fifth").html(Math.round(json.consolidated_weather[4].the_temp) + " C");
     })
     .catch((err) => console.error(err));
 });
